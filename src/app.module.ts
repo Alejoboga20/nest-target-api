5fe04f294be94a0f1811686ac2b5ffab8829cb27
Alejo Boga <alejoboga20@gmail.com>
2022-08-09 15:44:32 -0300

    Setup SQL Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'MySecretPassword123',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
