aea6f0e9db0efcb140cf28cc5d1bf9ab77c0f96d
Alejo Boga <alejoboga20@gmail.com>
2022-08-09 15:27:07 -0300

    Cleaning resource

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Post('/signup')
+  signup(@Body() createUserDto: CreateUserDto) {
+    return this.userService.signup(createUserDto);
+  }
+
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  signin() {
+    return 'Login with existing user';
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Post()
+  logout() {
+    return 'Logout';
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private users: CreateUserDto[] = [];
+
+  signup(createUserDto: CreateUserDto) {
+    return this.users.push(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  signin() {
+    return `login`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  logout() {
+    return `logout`;
   }
 }
