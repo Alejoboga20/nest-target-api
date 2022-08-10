a75e50cf1b14af61450e9ba04a1c6dea81fc6e9b
Alejo Boga <alejoboga20@gmail.com>
2022-08-10 09:31:38 -0300

    Add user entity to DB

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  uid: string;
+
+  @Column()
+  provider: string;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  created_at: string;
+
+  @Column()
+  updated_at: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  push_token: string;
+
+  @Column()
+  allow_password_change: boolean;
+
+  //TODO: Avatar (tabla con imagenes)
+}
